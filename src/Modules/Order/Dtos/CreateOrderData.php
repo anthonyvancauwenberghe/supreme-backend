7cda278514460ddee0dab159fc1c43c44db3ca6c
root <root@supremewatcher.api.supremewatcher.io>
2019-07-04 11:37:44 +0000

    orderdto

--- src/Modules/Order/Dtos/CreateOrderData.php
+++ src/Modules/Order/Dtos/CreateOrderData.php
@@ -7,17 +7,17 @@ use Foundation\Abstracts\Dtos\Dto;
 class CreateOrderData extends Dto
 {
     /**
-     * @var int $item_id
+     * @var $item_id
      */
     public $item_id;
 
     /**
-     * @var int $style_id
+     * @var $style_id
      */
     public $style_id;
 
     /**
-     * @var int $size_id
+     * @var $size_id
      */
     public $size_id;
 
